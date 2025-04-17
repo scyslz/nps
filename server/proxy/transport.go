752698e42f3a6d82d41dca393917aaefdbc6394a
D-Jy <duan@d-jy.net>
2025-04-18 05:37:39 +0800

    重写透明代理

--- server/proxy/transport.go
+++ server/proxy/transport.go
@@ -4,9 +4,11 @@
 package proxy
 
 import (
+	"fmt"
 	"net"
 	"strconv"
 	"syscall"
+	"unsafe"
 
 	"github.com/djylb/nps/lib/common"
 	"github.com/djylb/nps/lib/conn"
@@ -24,23 +26,65 @@ func HandleTrans(c *conn.Conn, s *TunnelModeServer) error {
 const SO_ORIGINAL_DST = 80
 
 func getAddress(conn net.Conn) (string, error) {
-	sysrawconn, f := conn.(syscall.Conn)
-	if !f {
-		return "", nil
+	sysconn, ok := conn.(syscall.Conn)
+	if !ok {
+		return "", fmt.Errorf("connection does not support SyscallConn")
 	}
-	rawConn, err := sysrawconn.SyscallConn()
+	raw, err := sysconn.SyscallConn()
 	if err != nil {
-		return "", nil
+		return "", err
 	}
-	var ip string
-	var port uint16
-	err = rawConn.Control(func(fd uintptr) {
-		addr, err := syscall.GetsockoptIPv6Mreq(int(fd), syscall.IPPROTO_IP, SO_ORIGINAL_DST)
-		if err != nil {
+
+	var dst string
+	var opErr error
+
+	err = raw.Control(func(fd uintptr) {
+		// IPv4
+		var sa4 syscall.RawSockaddrInet4
+		sz4 := uint32(unsafe.Sizeof(sa4))
+		_, _, errno4 := syscall.Syscall6(
+			syscall.SYS_GETSOCKOPT,
+			fd,
+			uintptr(syscall.SOL_IP),
+			uintptr(SO_ORIGINAL_DST),
+			uintptr(unsafe.Pointer(&sa4)),
+			uintptr(unsafe.Pointer(&sz4)),
+			0,
+		)
+		if errno4 == 0 {
+			ip := net.IPv4(sa4.Addr[0], sa4.Addr[1], sa4.Addr[2], sa4.Addr[3])
+			port := int(sa4.Port>>8)&0xff | int(sa4.Port&0xff)<<8
+			dst = ip.String() + ":" + strconv.Itoa(port)
 			return
 		}
-		ip = net.IP(addr.Multiaddr[4:8]).String()
-		port = uint16(addr.Multiaddr[2])<<8 + uint16(addr.Multiaddr[3])
+
+		// IPv6
+		var sa6 syscall.RawSockaddrInet6
+		sz6 := uint32(unsafe.Sizeof(sa6))
+		_, _, errno6 := syscall.Syscall6(
+			syscall.SYS_GETSOCKOPT,
+			fd,
+			uintptr(syscall.SOL_IPV6),
+			uintptr(SO_ORIGINAL_DST),
+			uintptr(unsafe.Pointer(&sa6)),
+			uintptr(unsafe.Pointer(&sz6)),
+			0,
+		)
+		if errno6 == 0 {
+			ip := net.IP(sa6.Addr[:])
+			port := int(sa6.Port>>8)&0xff | int(sa6.Port&0xff)<<8
+			dst = "[" + ip.String() + "]:" + strconv.Itoa(port)
+			return
+		}
+
+		opErr = fmt.Errorf("not a redirected connection (errno4=%v, errno6=%v)", errno4, errno6)
 	})
-	return ip + ":" + strconv.Itoa(int(port)), nil
+
+	if err != nil {
+		return "", err
+	}
+	if opErr != nil {
+		return "", opErr
+	}
+	return dst, nil
 }
