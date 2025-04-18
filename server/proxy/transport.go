25bcf63b7bc7194926ccada57e7fcf6b77a59f34
D-Jy <duan@d-jy.net>
2025-04-18 08:04:23 +0800

    替换掉特定平台变量

--- server/proxy/transport.go
+++ server/proxy/transport.go
@@ -40,45 +40,38 @@ func getAddress(conn net.Conn) (string, error) {
 	var opErr error
 
 	err = raw.Control(func(fd uintptr) {
-		// IPv4
-		var sa4 unix.RawSockaddrInet4
-		sz4 := uint32(unsafe.Sizeof(sa4))
-		_, _, errno4 := unix.Syscall6(
+		var sa unix.RawSockaddrAny
+		sz := uint32(unsafe.Sizeof(sa))
+		_, _, errno := unix.Syscall6(
 			unix.SYS_GETSOCKOPT,
 			fd,
-			uintptr(unix.SOL_IP),
+			uintptr(unix.SOL_SOCKET),
 			uintptr(SO_ORIGINAL_DST),
-			uintptr(unsafe.Pointer(&sa4)),
-			uintptr(unsafe.Pointer(&sz4)),
+			uintptr(unsafe.Pointer(&sa)),
+			uintptr(unsafe.Pointer(&sz)),
 			0,
 		)
-		if errno4 == 0 {
-			ip := net.IPv4(sa4.Addr[0], sa4.Addr[1], sa4.Addr[2], sa4.Addr[3])
-			port := int(sa4.Port>>8)&0xff | int(sa4.Port&0xff)<<8
-			dst = ip.String() + ":" + strconv.Itoa(port)
+		if errno == 0 {
+			switch sa.Addr.Family {
+			case unix.AF_INET:
+				// IPv4
+				sa4 := (*unix.RawSockaddrInet4)(unsafe.Pointer(&sa))
+				ip := net.IPv4(sa4.Addr[0], sa4.Addr[1], sa4.Addr[2], sa4.Addr[3])
+				port := int(sa4.Port>>8)&0xff | int(sa4.Port&0xff)<<8
+				dst = ip.String() + ":" + strconv.Itoa(port)
+			case unix.AF_INET6:
+				// IPv6
+				sa6 := (*unix.RawSockaddrInet6)(unsafe.Pointer(&sa))
+				ip := net.IP(sa6.Addr[:])
+				port := int(sa6.Port>>8)&0xff | int(sa6.Port&0xff)<<8
+				dst = "[" + ip.String() + "]:" + strconv.Itoa(port)
+			default:
+				opErr = fmt.Errorf("unsupported address family: %d", sa.Addr.Family)
+			}
 			return
 		}
 
-		// IPv6
-		var sa6 unix.RawSockaddrInet6
-		sz6 := uint32(unsafe.Sizeof(sa6))
-		_, _, errno6 := unix.Syscall6(
-			unix.SYS_GETSOCKOPT,
-			fd,
-			uintptr(unix.SOL_IPV6),
-			uintptr(SO_ORIGINAL_DST),
-			uintptr(unsafe.Pointer(&sa6)),
-			uintptr(unsafe.Pointer(&sz6)),
-			0,
-		)
-		if errno6 == 0 {
-			ip := net.IP(sa6.Addr[:])
-			port := int(sa6.Port>>8)&0xff | int(sa6.Port&0xff)<<8
-			dst = "[" + ip.String() + "]:" + strconv.Itoa(port)
-			return
-		}
-
-		opErr = fmt.Errorf("not a redirected connection (errno4=%v, errno6=%v)", errno4, errno6)
+		opErr = fmt.Errorf("not a redirected connection (errno=%v)", errno)
 	})
 
 	if err != nil {
