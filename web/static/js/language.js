0ccd5fa32148d7abfc8ec140d8bf6c0dc4466847
D-Jy <duan@d-jy.net>
2025-05-02 22:53:04 +0800

    添加翻译，简化确认逻辑

--- web/static/js/language.js
+++ web/static/js/language.js
@@ -159,12 +159,12 @@ function submitform(action, url, postdata) {
         }
     });
     switch (action) {
-        case 'start':
-        case 'stop':
         case 'delete':
             var langobj = languages['content']['confirm'][action];
             action = (langobj[languages['current']] || langobj[languages['default']] || 'Are you sure you want to ' + action + ' it?');
             if (!confirm(action)) return;
+        case 'start':
+        case 'stop':
             postsubmit = true;
         case 'add':
         case 'edit':
@@ -173,17 +173,17 @@ function submitform(action, url, postdata) {
                 url: url,
                 data: postdata,
                 success: function (res) {
-                    $('#alertModal').off('hidden.bs.modal').on('hidden.bs.modal', function (e) {
-                        if (res.status) {
-                            if (postsubmit) {
-                                document.location.reload();
-                            } else {
-                                window.location.href = document.referrer
-                            }
-                        }
-                    });
-                    $('#alertModalContent').text(langreply(res.msg));
-                    $('#alertModal').modal();
+                    if (res.status) {
+                        showMsg(langreply(res.msg), 'success', 1500, function() {
+                          if (postsubmit) {
+                            document.location.reload();
+                          } else {
+                            window.location.href = document.referrer;
+                          }
+                        });
+                    } else {
+                        showMsg(langreply(res.msg), 'error', 5000);
+                    }
                 }
             });
             return;
@@ -193,13 +193,13 @@ function submitform(action, url, postdata) {
                 url: url,
                 data: postdata,
                 success: function (res) {
-                    $('#alertModal').off('hidden.bs.modal').on('hidden.bs.modal', function (e) {
-                        if (res.status) {
-                            document.location.reload();
-                        }
-                    });
-                    $('#alertModalContent').text(langreply(res.msg));
-                    $('#alertModal').modal();
+                    if (res.status) {
+                        msgSuccess(langreply(res.msg), 1500, function() {
+                          document.location.reload();
+                        });
+                    } else {
+                        showMsg(langreply(res.msg), 'error', 5000);
+                    }
                 }
             });
     }
@@ -233,5 +233,33 @@ function oCopy(obj){
     tempInput.select();
     document.execCommand('copy');
     document.body.removeChild(tempInput);
-    msg.success('Copied!');
+    showMsg(langreply('Copied'));
 }
+
+function showMsg(text, type = 'success', dur = 1500, cb) {
+    var old = document.getElementById('wangmarket_loading');
+    if (old) old.parentNode.removeChild(old);
+    var isLong = text && text.length > 10;
+    var svg = type === 'error'
+      ? '<svg style="width:3rem;height:3rem;padding:1.5rem;padding-bottom:1.1rem;box-sizing:content-box;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6977"><path d="M696.832 326.656c-12.8-12.8-33.28-12.8-46.08 0L512 465.92 373.248 327.168c-12.8-12.8-33.28-12.8-46.08 0s-12.8 33.28 0 46.08L466.432 512l-139.264 139.264c-12.8 12.8-12.8 33.28 0 46.08s33.28 12.8 46.08 0L512 558.08l138.752 139.264c12.288 12.8 32.768 12.8 45.568 0.512l0.512-0.512c12.8-12.8 12.8-33.28 0-45.568L557.568 512l139.264-139.264c12.8-12.8 12.8-33.28 0-46.08 0 0.512 0 0 0 0zM512 51.2c-254.464 0-460.8 206.336-460.8 460.8s206.336 460.8 460.8 460.8 460.8-206.336 460.8-460.8-206.336-460.8-460.8-460.8z m280.064 740.864c-74.24 74.24-175.104 116.224-280.064 115.712-104.96 0-205.824-41.472-280.064-115.712S115.712 616.96 115.712 512s41.472-205.824 116.224-280.064C306.176 157.696 407.04 115.712 512 116.224c104.96 0 205.824 41.472 280.064 116.224 74.24 74.24 116.224 175.104 115.712 280.064 0.512 104.448-41.472 205.312-115.712 279.552z" fill="#ffffff" p-id="6978"></path></svg>'
+      : '<svg style="width:3rem;height:3rem;padding:1.5rem;padding-bottom:1.1rem;box-sizing:content-box;" viewBox="0 0 1024 1024"><path d="M384 887.456L25.6 529.056 145.056 409.6 384 648.544 878.944 153.6 998.4 273.056z" fill="#ffffff"/></svg>';
+    var w = document.createElement('div');
+    w.id = 'wangmarket_loading';
+    w.style = 'position: fixed;z-index: 2147483647;margin: 0 auto;text-align: center;width: 100%;';
+    w.innerHTML =
+      '<div id="loading" style="position: fixed;top: 30%;text-align: center;font-size: 1rem;color: #dedede;margin: 0px auto;left: 50%;margin-left: -'+(isLong?'9':'3.5')+'rem;">'
+        +'<div style="width: 7rem;background-color: #2e2d3c;border-radius: 0.3rem;filter: alpha(Opacity=80);-moz-opacity:0.8;opacity:0.8;min-height:4.8rem;'+(isLong?'width: 18rem;':'')+'">'
+          +'<div'+(isLong?' style="float:left;height:20rem;margin-top:-0.6rem;position:fixed;"':'')+'>'+svg+'</div>'
+          +'<div style="width:100%;padding-bottom:1.4rem;font-size:1.1rem;padding-left:0.3rem;padding-right:0.3rem;box-sizing:border-box;line-height:1.2rem;color:white;'
+              +(isLong?'padding:1rem;text-align:left;padding-right:0.3rem;line-height:1.5rem;margin-left:4.8rem;padding-right:5.5rem;padding-top:0.7rem;':'')+'">'
+            + text +
+          '</div>'
+        +'</div>'
+      +'</div>';
+    document.body.appendChild(w);
+    setTimeout(function(){
+        w.parentNode.removeChild(w);
+        if (typeof cb === 'function') cb();
+    }, dur);
+}
+
